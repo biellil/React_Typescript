1c7bdf820393801a027c4989f5232a704ed364b9
biel.lil <78098857+biellil@users.noreply.github.com>
2023-04-03 16:12:41 -0300

    03/04

--- gitcollection/src/App.tsx
+++ gitcollection/src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
-
+import { GlobalStyle } from './styles/global';
 import { routes as Routes } from './Routes';
 
 
 
 const App: React.FC = ()=> {
   return (
-
-    <BrowserRouter>
-    <Routes/>    
+    <>
+      <BrowserRouter>
+        <Routes />
       </BrowserRouter>
-    )
+      <GlobalStyle />
+    </>
+  );
 };
 
 
