1ff2400338d5549dd1abfdc7a782f35358c4947f
biel.lil <78098857+biellil@users.noreply.github.com>
2023-04-19 16:00:33 -0300

    19/04
    Nota de atualização 19/04: Hoje eu estilizei a página "repo" da aplicação "git collection" utilizando a importação do styled-components com o comando "import styled from 'styled-components';".

--- gitcollection/src/Routes/index.tsx
+++ gitcollection/src/Routes/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom'
 import { Dashboard } from '../pages/Dashboard';
-import { Repositorio } from '../pages/Repo';
+import { Repo } from '../pages/Repo';
 
 export const routes: React.FC = ()=> {
   return (
     <Switch>
         <Route component={Dashboard} path ='/' exact/>
-        <Route component={Repositorio} path="/repositories/:repository+" />
+        <Route component={Repo} path="/repositories/:repository+" />
         </Switch>
     )
 };
--- gitcollection/src/pages/Repo/index.tsx
+++ gitcollection/src/pages/Repo/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import {useRouteMatch} from 'react-router-dom'
-import { FiChevronLeft } from 'react-icons/fi';
-
-import { Header } from './styles';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepoInfo, Issues } from './styles';
 import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
 }
-
-export const Repositorio: React.FC = ()=> {
+export const Repo: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-
   return (
     <>
       <Header>
@@ -20,11 +17,41 @@ export const Repositorio: React.FC = ()=> {
           <FiChevronLeft />
           Voltar
         </a>
-        <h1>Repo: {params.repository}</h1>
       </Header>
+
+      <RepoInfo>
+        <header>
+          <img src="" alt="Aluizio Developer" />
+          <div>
+            <strong>aluiziodeveloper/mini-curso-reactjs</strong>
+            <p>Repositorio do mini curso gratuito de reactjs</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>2330</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>210</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>79</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepoInfo>
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>f;lkj lkfjdlkfjk ldjsffjdlkfj</strong>
+            <p>sdfkjdslkfjdflkdjsflkdjfdjfksdjf</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </>
   );
 };
-
-
-
