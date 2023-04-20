2455a4ab1a232e127059843c07ca387a52f848e3
biel.lil <78098857+biellil@users.noreply.github.com>
2023-04-20 12:43:51 -0300

    20/04
    Nota de atualização 20/04: Hoje, alterei os dados de uma fonte fixa para uma fonte dinâmica na página "repo". Para isso, consumi a API do GitHub utilizando o hook useEffect do React.js.

--- gitcollection/src/pages/Repo/index.tsx
+++ gitcollection/src/pages/Repo/index.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
-import { Link, useRouteMatch } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { Header, RepoInfo, Issues } from './styles';
 import logo from '../../assets/logo.svg';
+import {api} from '../../services/api';
 
 interface RepositoryParams {
   repository: string;
 }
+interface GithubRepository {
+  full_name: string;
+  description: string;
+  forks_count: number;
+  open_issues_count: number;
+  stargazers_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface GithubIssue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
+
 export const Repo: React.FC = () => {
+  const [repository, setRepository] =
+  React.useState<GithubRepository | null>(null);
+  const [issues, setIssues] = React.useState<GithubIssue[]>([]);
   const { params } = useRouteMatch<RepositoryParams>();
+
+  React.useEffect(() => {
+    api
+      .get(`repos/${params.repository}`)
+      .then(response => setRepository(response.data));
+
+    api
+      .get(`repos/${params.repository}/issues`)
+      .then(response => setIssues(response.data));
+  }, [params.repository]);
+
+  
+
   return (
     <>
       <Header>
@@ -18,39 +57,45 @@ export const Repo: React.FC = () => {
           Voltar
         </a>
       </Header>
-
-      <RepoInfo>
-        <header>
-          <img src="" alt="Aluizio Developer" />
-          <div>
-            <strong>aluiziodeveloper/mini-curso-reactjs</strong>
-            <p>Repositorio do mini curso gratuito de reactjs</p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong>2330</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>210</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>79</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </RepoInfo>
+      {repository && (
+        <RepoInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepoInfo>
+      )}
       <Issues>
-        <Link to="/">
-          <div>
-            <strong>f;lkj lkfjdlkfjk ldjsffjdlkfj</strong>
-            <p>sdfkjdslkfjdflkdjsflkdjfdjfksdjf</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </Link>
+        {issues.map(issues =>(
+            <a href={issues.html_url} key={issues.id}>
+            <div>
+              <strong>{issues.title}</strong>
+              <p>{issues.user.login}</p>
+            </div>
+  
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   );
